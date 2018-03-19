94abd33c90cc4b17d15588612d46e9c919b5bc73
pertruccio <1328703+pertruccio@users.noreply.github.com>
2018-03-19 15:04:55 +0100

    added comments to convert_list_to_sql.sh

--- convert_list_to_sql.sh
+++ convert_list_to_sql.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+# delete all lines in existing table, they are probably old
 echo "TRUNCATE TABLE tlspolicies;"
+
+# for each line in tls_policy-dane file
 while read p; do
 	if [[ $p != \#* ]]; then
+		# if it's not a comment build a sql statement
 		stringarray=($p)
 		NEW="insert into tlspolicies (domain, policy, params) values ('${stringarray[0]}', '${stringarray[1]}', '${stringarray[2]}');"
 		echo $NEW
 	fi
 done <./tls_policy-dane
+
+# for now this scripts echoes to STDOUT. better put it in file!
